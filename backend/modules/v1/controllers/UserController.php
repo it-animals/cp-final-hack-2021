93995a89c54a9b95a9668e5a81450d465b9bf59e
Dmitrii N <dmitrii@dev>
2021-12-02 22:16:22 +0400

    api register user

--- backend/modules/v1/controllers/UserController.php
+++ backend/modules/v1/controllers/UserController.php
@@ -3,12 +3,16 @@
 namespace app\modules\v1\controllers;
 
 use app\models\LoginForm;
+use app\models\User;
 use app\modules\v1\helpers\BehaviorHelper;
 use app\modules\v1\traits\GetUserTrait;
 use app\modules\v1\traits\OptionsActionTrait;
 use Yii;
 use yii\rest\Controller;
+use yii\web\BadRequestHttpException;
 use yii\web\ForbiddenHttpException;
+use function mb_strtolower;
+use function trim;
 
 class UserController extends Controller
 {
@@ -18,12 +22,32 @@ class UserController extends Controller
     {
         return BehaviorHelper::api(parent::behaviors(), [
             'POST' => [
-                BehaviorHelper::AUTH_NOT_REQUIRED => ['login'],
+                BehaviorHelper::AUTH_NOT_REQUIRED => ['login', 'register'],
             ],
             'GET' => [
                 BehaviorHelper::AUTH_REQUIRED => ['info'],
             ],
         ]);
+    }
+
+    public function actionRegister()
+    {
+        $request = Yii::$app->request;
+
+        $user = new User();
+        $user->fio = $request->getBodyParam('fio');
+        $user->password = Yii::$app->security->generatePasswordHash($request->getBodyParam('password'));
+        $user->email = trim(mb_strtolower($request->getBodyParam('email'), 'UTF-8'));
+        $user->role = $request->getBodyParam('role');
+
+        if (!$user->save()) {
+            throw new BadRequestHttpException($user->getErrorSummary(false)[0]);
+        }
+        return [
+            'user' => [
+                'email' => $user->email,
+            ]
+        ];
     }
 
     public function actionLogin()
