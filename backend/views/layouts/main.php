324446ec60c0948827e2bb66e1daedaabad3257e
Dmitrii N <dmitrii@dev>
2021-12-03 16:43:43 +0400

    update layout

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -69,7 +69,7 @@ AppAsset::register($this);
 
 <footer class="footer mt-auto py-3 text-muted">
     <div class="container">
-        <p class="float-left">&copy; My Company <?= date('Y') ?></p>
+        <p class="float-left">&copy; IT-animals <?= date('Y') ?></p>
         <p class="float-right"><?= Yii::powered() ?></p>
     </div>
 </footer>
